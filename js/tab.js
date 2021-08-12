76bfbc8071ae8838022ca5903b745c370b7b9d8c
linupy <linupy@163.com>
2021-08-12 19:34:46 +0800

    [feat] get tabs info

--- js/tab.js
+++ js/tab.js
@@ -1,2 +1,19 @@
 var info = document.getElementById("tab")
-info.innerHTML = "developing..."
+
+var tabs_array = []
+
+chrome.tabs.query({}, function (tabArray) {
+  for (const tabElement of tabArray) {
+    item = {}
+    item.id = tabElement.id
+    item.windowId = tabElement.windowId
+    item.favIconUrl = tabElement.favIconUrl
+    item.title = tabElement.title
+    item.url = tabElement.url
+    tabs_array.push(item)
+  }
+  info.innerHTML = JSON.stringify(tabs_array)
+})
+
+
+
